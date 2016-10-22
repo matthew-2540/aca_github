fe8c9e2ee2d9dc953c2a2db6c4aab86007ce1f28
UCA <UCA@161.31.58.170>
2016-10-22 10:57:29 -0500

    added method to say hello from me

--- src/com/arkansascodingacademy/example/HelloWorld.java
+++ src/com/arkansascodingacademy/example/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+    private void pownall(String[] args){
+        System.out.println("Karen says Hello");
+    }
 }
