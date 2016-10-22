ef38e18afe06a960584ea5d9c195ce7ed436602c
UCA <UCA@161.31.58.42>
2016-10-22 11:03:48 -0500

    1st try at committing on branch

--- src/com/arkansascodingacademy/example/HelloWorld.java
+++ src/com/arkansascodingacademy/example/HelloWorld.java
@@ -6,5 +6,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+   
 }
+        {
+        private void dianne (); 
+            System.out.println("Hello Dianne");
+        }
+   
+    
+
