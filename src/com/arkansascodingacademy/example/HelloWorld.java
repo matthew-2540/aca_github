de4a85858f3feee5e1b6cabff50f83303ceace1b
UCA <UCA@161.31.58.225>
2016-10-22 11:01:42 -0500

    Commit1

--- src/com/arkansascodingacademy/example/HelloWorld.java
+++ src/com/arkansascodingacademy/example/HelloWorld.java
@@ -1,11 +1,12 @@
 package com.arkansascodingacademy.example;
 
 
+@SuppressWarnings("InitializerMayBeStatic")
 public class HelloWorld {
     
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
     
-    
 }
+//Commit #1//
